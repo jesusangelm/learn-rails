c71a43bf3bb1eaf1a8707953e57cb587cdf4c236
Jesus Marin <jesusangelm@gmail.com>
2014-01-07 20:30:07 -0430

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
+    flash.now[:notice] = "Bienvenido!"
+    flash.now[:alert] = "Mi cumpleaños esta cerca..."
   end
   
 end
