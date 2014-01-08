d0f46d9af73663afb80233d826d2f8df655ae16e
Jesus Marin <jesusangelm@gmail.com>
2014-01-08 10:28:10 -0430

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,8 +2,6 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
-    flash.now[:notice] = "Bienvenido!"
-    flash.now[:alert] = "Mi cumpleaños esta cerca..."
   end
   
 end
